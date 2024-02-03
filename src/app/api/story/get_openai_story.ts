2d9d4601a0de2c922d3c734eb6f40a49f38ffbd1
Dane Walters <dwalters.maruko@gmail.com>
2024-02-03 18:36:27 +0800

    feat: async voice

--- src/app/api/story/get_openai_story.ts
+++ src/app/api/story/get_openai_story.ts
@@ -1,21 +1,22 @@
-import OpenAI from 'openai'
-import { env } from 'process'
+import OpenAI from "openai";
 
-const openai = new OpenAI()
+const openai = new OpenAI();
 
 export async function getOpenaiStory(storyType: string) {
-  const contentStory = `生成一个50字的${storyType}`
+  const contentStory = `生成一个200字的${storyType}`;
 
   const completion = await openai.chat.completions.create({
     messages: [
-      { role: 'system', content: '你是一个生成故事的助手.' },
+      { role: "system", content: "你是一个生成故事的助手." },
       {
-        role: 'user',
+        role: "user",
         content: contentStory,
       },
     ],
-    model: 'gpt-3.5-turbo',
-  })
+    model: "gpt-3.5-turbo",
+  });
+  console.log(completion.choices[0]);
+  
 
-  return completion.choices[0].message.content
+  return completion.choices[0].message.content;
 }
