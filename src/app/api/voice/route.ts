e96ba0cb66baa9c7aa05b9509e85d9bd5123936d
Dane Walters <dwalters.maruko@gmail.com>
2024-02-03 11:14:42 +0800

    fixup! feat: get voice list

--- src/app/api/voice/route.ts
+++ src/app/api/voice/route.ts
@@ -1,10 +1,23 @@
+import { SelectMessage, SelectOption } from "@/types/message";
 import { getVoiceList } from "./get_voice_list";
+import { log } from "console";
 
 export async function GET(request: Request) {
   const voiceList = await getVoiceList();
 
-  if(voiceList) {
-    return voiceList
+  if (voiceList) {
+    const res = await voiceList.json();
+    const data: SelectOption<string>[] = res.data.map((
+      d: { name: any; id: any },
+    ) => ({ label: d.name, value: d.id }));
+    const select: SelectMessage = {
+      type: "select",
+      id: crypto.randomUUID(),
+      data,
+      isBot: true,
+    };
+
+    return Response.json(select);
   }
-  return Response.json({message: "not found voice"})
+  return Response.json({ message: "not found voice" });
 }
